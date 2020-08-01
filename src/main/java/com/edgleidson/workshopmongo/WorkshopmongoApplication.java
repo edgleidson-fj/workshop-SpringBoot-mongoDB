81bd40473f0e0d26442efe04344b828148a0e92f
edgleidson-fj <wake-up88@hotmail.com>
2020-08-01 20:45:21 -0300

    Primeiro commit e projeto criado

---
+++ src/main/java/com/edgleidson/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.edgleidson.workshopmongo;
+//Aula 342 - Primeiro commit e projeto criado.
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
